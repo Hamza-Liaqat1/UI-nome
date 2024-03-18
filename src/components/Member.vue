436294ca703dba6030de098faa769c9adea677a4
Hamza <hamzaliaqat1481@gmail.com>
2024-03-19 02:31:40 +0500

    🐛 FIX: build error

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -168,7 +168,7 @@ import { getAddress, sendBtcTransaction } from 'sats-connect';
 // import sha256 from 'crypto-js/sha256';
 import { event } from 'vue-gtag'
 import imageCompressor from 'vue-image-compressor'
-import {available_rarity} from "../constants/rarity"
+import { available_rarity } from '@/constants/rarity';
 export default {
   name: 'Home',
   data() {
